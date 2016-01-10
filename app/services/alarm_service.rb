2fe9fa651350f30cc3a388f4d595949e4b7f9a29
youcune <you@nakanishi.email>
2016-01-10 17:07:42 +0900

    アラームを鳴らすのは午前中のみ

--- app/services/alarm_service.rb
+++ app/services/alarm_service.rb
@@ -4,6 +4,6 @@ class AlarmService
 
   def self.set(prior_time = PRIOR_TIME)
     alarm_time = GoogleCalendarService.today_first_event_start_time - prior_time
-    AlarmJob.set(wait_until: alarm_time).perform_later
+    AlarmJob.set(wait_until: alarm_time).perform_later if alarm_time.am?
   end
 end
