8eaa6785b1bf96269abd5815707ef1260646faf1
youcune <you@nakanishi.email>
2016-06-19 08:49:35 +0900

    rails consoleから呼ぶ用にIRKitのget_messagesを呼ぶ機能を追加

--- app/services/ir_kit_service.rb
+++ app/services/ir_kit_service.rb
@@ -2,4 +2,8 @@ class IRKitService
   def self.post_data(data)
     IRKit::Device.new(address: 'irkit').post_messages(format: 'raw', freq: 38, data: data)
   end
+
+  def self.get_data
+    IRKit::Device.new(address: 'irkit').get_messages.data.to_s
+  end
 end
