687ac1d2fce98411b5c7b57feed09535de34ef18
youcune <you@nakanishi.email>
2016-06-19 08:37:01 +0900

    sleepいらない気がした

--- app/services/ir_kit_service.rb
+++ app/services/ir_kit_service.rb
@@ -1,8 +1,5 @@
 class IRKitService
   def self.post_data(data)
     IRKit::Device.new(address: 'irkit').post_messages(format: 'raw', freq: 38, data: data)
-
-    # 連続して送ると届かないっぽいので休憩
-    sleep 2
   end
 end
